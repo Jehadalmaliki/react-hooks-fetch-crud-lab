2c9d400812b45a945a751c7140cb3435cad68bb2
Jehad Almaliki <yourgitemail@example.com>
2023-03-23 00:06:04 +0300

    done solution

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React ,{useState,useEffect}from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState([]);
+
+  // Add useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul> {items.map((item) => (
+        <>
+    <li>{item.prompt}</li>
+    <li>{item.answers.map((item) => (<li>{item}</li>))}</li></>
+        ))}</ul>
     </section>
   );
 }
